a57198ac09821161f10e84232b6f53ef964af5ea
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-04 13:13:31 +0530

    modified

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,24 +2,25 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={Link} to="/">
             <img src={logo} alt={'logo'} />
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart />
                 &nbsp;Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser />
                 &nbsp;Sign In
               </Nav.Link>
