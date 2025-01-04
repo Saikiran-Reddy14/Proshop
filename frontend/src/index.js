bea0a84377c0157ea1236b0ad323e4f157f936fc
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-04 13:14:12 +0530

    added routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import HomePage from './pages/HomePage';
+import ProductDetails from './pages/ProductDetails';
+import ErrorPage from './pages/ErrorPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index={true} path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
