96add655673a1fbb34b0dacda607efa7807c57a9
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-07 17:44:15 +0530

    added routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/productModel');
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,15 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const products = require('./data/products');
 const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
 connectDB();
 
+app.use('/api/products', productRoutes);
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/product/:id', (req, res) => {
-  const product = products?.default?.filter((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 app.get('*', (req, res) => {
   res.status(404).send('Not Found');
