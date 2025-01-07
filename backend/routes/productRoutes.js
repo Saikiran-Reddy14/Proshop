4a1b5a8c41f74356da0373a85681e4fce35ef89c
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-07 18:57:46 +0530

    added productController for backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/productModel');
-const asyncHandler = require('../middleware/asyncHandler');
+const {
+  getProducts,
+  getProductById,
+} = require('../controllers/productController');
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    if (products.length === 0) {
-      res.status(404);
-      throw new Error('No products found');
-    }
-    res.status(200).json(products);
-  })
-);
+router.route('/').get(getProducts);
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    if (!product) {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-    res.status(200).json(product);
-  })
-);
+router.route('/:id').get(getProductById);
 
 module.exports = router;
