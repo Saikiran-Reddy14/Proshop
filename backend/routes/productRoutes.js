261064072ecdb5c4bf5d5001555d99f1a5ebe25e
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-07 17:57:41 +0530

    added routes in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/productModel');
+const asyncHandler = require('../middleware/asyncHandler');
 
-router.get('/', async (req, res) => {
-  try {
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    if (products.length === 0) {
+      res.status(404).json({ message: 'No products found' });
+    }
     res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
+  })
+);
 
-router.get('/:id', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404).json({ message: 'Product not found' });
+    }
     res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
+  })
+);
 
 module.exports = router;
