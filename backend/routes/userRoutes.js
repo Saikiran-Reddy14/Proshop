1cceaabdaaaa0eefba49b0049e5d259cafe4bbdc
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-12 21:11:28 +0530

    Added separate POST /register route for user registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,7 +13,8 @@ const {
 } = require('../controllers/userController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
-router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.route('/').get(protect, admin, getUsers);
+router.post('/register', registerUser);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
 router
