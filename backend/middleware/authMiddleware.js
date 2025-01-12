2c4e5b94083a283de84ad36860a8d191aff345ed
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-12 15:54:41 +0530

    Used decoded.id in protect middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,11 +10,9 @@ const protect = asyncHandler(async (req, res, next) => {
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select('-password');
+      req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
-      console.log(error.name);
-
       res.status(401);
       throw new Error('Not authorized: Invalid or expired token.');
     }
