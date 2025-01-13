fbacd16d20df2702c5b3e89868b31cbf51ead7f1
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-13 19:24:40 +0530

    Added CheckoutSteps component to ShippingPage

--- frontend/src/pages/ShippingPage.js
+++ frontend/src/pages/ShippingPage.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { saveShippingAddress } from '../slices/cartSlice';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingPage = () => {
   const cart = useSelector((state) => state.cart);
@@ -39,6 +40,7 @@ const ShippingPage = () => {
       </Link>
 
       <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
           <Form.Group controlId="address" className="my-2">
