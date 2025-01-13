ce3d2f6bfd3091b37a4853aed1767f2712434cdf
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-13 18:12:55 +0530

    Added saveShippingAddress action to save shipping address to the cart state

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
-const initialState = {
-  cartItems: JSON.parse(localStorage.getItem('cart'))?.cartItems || [],
-  itemsPrice: JSON.parse(localStorage.getItem('cart'))?.itemsPrice || 0,
-  shippingPrice: JSON.parse(localStorage.getItem('cart'))?.shippingPrice || 0,
-  totalPrice: JSON.parse(localStorage.getItem('cart'))?.totalPrice || 0,
-};
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 export const cartSlice = createSlice({
   name: 'cart',
@@ -25,34 +23,7 @@ export const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-
-      // Calculate items price (sum of item price * quantity)
-      const itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-        0
-      );
-
-      // Calculate shipping price (if itemsPrice >= 500, no shipping, otherwise 99)
-      const shippingPrice = itemsPrice >= 500 ? 0 : 99;
-
-      // Calculate total price (itemsPrice + shippingPrice)
-      const totalPrice = (itemsPrice + shippingPrice).toFixed(0);
-
-      // Update state with the new values
-      state.itemsPrice = itemsPrice;
-      state.shippingPrice = shippingPrice;
-      state.totalPrice = totalPrice;
-
-      // Save the updated cart object to localStorage
-      localStorage.setItem(
-        'cart',
-        JSON.stringify({
-          cartItems: state.cartItems,
-          itemsPrice: state.itemsPrice,
-          shippingPrice: state.shippingPrice,
-          totalPrice: state.totalPrice,
-        })
-      );
+      return updateCart(state, item);
     },
     removeFromCart: (state, action) => {
       const id = action.payload;
@@ -60,37 +31,16 @@ export const cartSlice = createSlice({
       // Remove item from cartItems
       state.cartItems = state.cartItems.filter((x) => x._id !== id);
 
-      // Calculate items price (sum of item price * quantity)
-      const itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-        0
-      );
-
-      // Calculate shipping price (if itemsPrice >= 500, no shipping, otherwise 99)
-      const shippingPrice = itemsPrice >= 500 ? 0 : 99;
-
-      // Calculate total price (itemsPrice + shippingPrice)
-      const totalPrice = itemsPrice + shippingPrice;
-
-      // Update state with the new values
-      state.itemsPrice = itemsPrice;
-      state.shippingPrice = shippingPrice;
-      state.totalPrice = totalPrice;
-
-      // Save the updated cart object to localStorage
-      localStorage.setItem(
-        'cart',
-        JSON.stringify({
-          cartItems: state.cartItems,
-          itemsPrice: state.itemsPrice,
-          shippingPrice: state.shippingPrice,
-          totalPrice: state.totalPrice,
-        })
-      );
+      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
     },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
