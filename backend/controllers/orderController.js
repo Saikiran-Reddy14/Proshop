6dc31fc32da000815f75b6cd3806b81e2c42477a
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-14 23:51:30 +0530

    Added functionality to create new orders, retrieve order details by ID, and get logged-in user's orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,38 +5,79 @@ const Order = require('../models/orderModel');
 // POST /api/orders
 // Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send('add order items');
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else if (!shippingAddress || !paymentMethod) {
+    res.status(400);
+    throw new Error('Shipping address and payment method are required');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // Get logged in user orders
 // GET /api/orders/myorders
 // Private
 const getMyOrderItems = asyncHandler(async (req, res) => {
-  res.send('get my orders');
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // Get order by ID
 // GET /api/orders/:id
 // Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send('get order by id');
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
-// Get Update order to paid
-// GET /api/orders/:id/pay
+// Update order to paid
+// PUT /api/orders/:id/pay
 // Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
   res.send('update order to paid');
 });
 
-// Get Update order to delivered
-// GET /api/orders/:id/deliver
+// Update order to delivered
+// PUT /api/orders/:id/deliver
 // Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('update order to delivered');
 });
 
-// Get Get all orders
+// Get all orders
 // GET /api/orders
 // Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
